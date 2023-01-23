677b906b7d6d919875bcfb6159e4e376e8e1cd54
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-23 01:03:06 +0100

    Update views.py

--- Base/views.py
+++ Base/views.py
@@ -16,3 +16,19 @@ def index(request):
     'punchline':punchline
   }
   return render(request, 'index.html', context)
+
+def random(request):
+  url = 'https://official-joke-api.appspot.com/random_joke'
+
+  response = requests.get(url)
+
+  data = response.json()
+
+  joke = data['setup']
+  punchline = data['punchline']
+
+  context = {
+    'joke': joke,
+    'punchline':punchline
+  }
+  return render(request, 'random.html', context)
